43f83fb14fd4054ec93f6d4099ce59fa4d6a4a1f
david-why <david_why@outlook.com>
2025-10-18 22:51:26 +0800

    add channel mapping

--- src/converter/discord.ts
+++ src/converter/discord.ts
@@ -1,13 +1,15 @@
 // from discord to slack
 
+import { GuildChannel } from 'discord.js'
 import { discord } from '../..'
-import { getUserByDiscord } from '../database'
+import { getMappingByDiscord, getUserByDiscord } from '../database'
 
 export async function discordToSlack(markdown: string) {
   // this still returns markdown, NOT mrkdwn!
   const replacements: Record<string, string> = {}
   for (const match of markdown.matchAll(/<@([0-9]+)>/g)) {
     const [segment, userId] = match
+    if (replacements[segment]) continue
     if (!userId) continue
     const user = await getUserByDiscord(userId)
     if (user) {
@@ -16,6 +18,25 @@ export async function discordToSlack(markdown: string) {
       const discordUser = await discord.users.fetch(userId)
       replacements[segment] = `@${discordUser.displayName}`
     }
+  }
+  for (const match of markdown.matchAll(/<#([0-9]+)>/g)) {
+    const [segment, channelId] = match
+    if (replacements[segment]) continue
+    if (!channelId) continue
+    const mapping = await getMappingByDiscord(channelId)
+    if (mapping) {
+      replacements[segment] = `<#${mapping.slack_channel}>`
+    } else {
+      const discordChannel = await discord.channels.fetch(channelId)
+      if (discordChannel && discordChannel instanceof GuildChannel) {
+        replacements[segment] = `#${discordChannel.name}`
+      } else {
+        replacements[segment] = `#(unknown)`
+      }
+    }
+  }
+  for (const [old, repl] of Object.entries(replacements)) {
+    markdown = markdown.replaceAll(old, repl)
   }
   return markdown
 }
